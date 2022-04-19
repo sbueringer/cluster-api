3834523bcbd66d82c305ffd9a4db3798f541fac8
killianmuldoon <47175296+killianmuldoon@users.noreply.github.com>
2022-04-20 00:32:44 +0100

    :seedling:  add defaulting and more validation to Extension webhook (#27)
    * add defaulting and more validation to Extension webhook
    
    Signed-off-by: killianmuldoon <kmuldoon@vmware.com>
    
    * address PR comments

--- exp/runtime/api/v1beta1/extension_webhook.go
+++ exp/runtime/api/v1beta1/extension_webhook.go
@@ -18,14 +18,18 @@ package v1beta1
 
 import (
 	"fmt"
+	"net/url"
 
 	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/util/validation"
 	"k8s.io/apimachinery/pkg/util/validation/field"
+	"k8s.io/utils/pointer"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/webhook"
 
+	clusterv1 "sigs.k8s.io/cluster-api/api/v1beta1"
 	"sigs.k8s.io/cluster-api/feature"
 )
 
@@ -47,51 +51,140 @@ func (e *Extension) Default() {
 	if e.Spec.NamespaceSelector == nil {
 		e.Spec.NamespaceSelector = &metav1.LabelSelector{}
 	}
+
+	// If a Service is defined default an unset port.
+	if e.Spec.ClientConfig.Service != nil {
+		if e.Spec.ClientConfig.Service.Port == nil {
+			e.Spec.ClientConfig.Service.Port = pointer.Int32(8443)
+		}
+	}
 }
 
 // ValidateCreate implements webhook.Validator so a webhook will be registered for the type.
 func (e *Extension) ValidateCreate() error {
-	// NOTE: Extensions is behind the RuntimeSDK feature gate flag; the web hook
-	// must prevent creating new objects in case the feature flag is disabled.
-	if !feature.Gates.Enabled(feature.RuntimeSDK) {
-		return field.Forbidden(
-			field.NewPath("spec"),
-			"can be set only if the RuntimeSDK feature flag is enabled",
-		)
-	}
-
-	// NOTE: Extensions is also behind the ClusterTopology feature gate flag; the web hook
-	// must prevent creating new objects in case the feature flag is disabled.
-	if !feature.Gates.Enabled(feature.ClusterTopology) {
-		return field.Forbidden(
-			field.NewPath("spec"),
-			"can be set only if the ClusterTopology feature flag is enabled",
-		)
-	}
-	return nil
+	return e.validate(nil)
 }
 
 // ValidateUpdate implements webhook.Validator so a webhook will be registered for the type.
 func (e *Extension) ValidateUpdate(old runtime.Object) error {
-	if !feature.Gates.Enabled(feature.RuntimeSDK) {
-		return field.Forbidden(
-			field.NewPath("spec"),
-			"can be set only if the RuntimeSDK feature flag is enabled",
-		)
-	}
-
-	// NOTE: Extensions is also behind the ClusterTopology feature gate flag; the web hook
-	// must prevent creating new objects in case the feature flag is disabled.
-	if !feature.Gates.Enabled(feature.ClusterTopology) {
-		return field.Forbidden(
-			field.NewPath("spec"),
-			"can be set only if the ClusterTopology feature flag is enabled",
-		)
-	}
-	if _, ok := old.(*Extension); !ok {
+	newExtension, ok := old.(*Extension)
+	if !ok {
 		return apierrors.NewBadRequest(fmt.Sprintf("expected an Extension but got a %T", old))
 	}
+	return e.validate(newExtension)
+}
+
+func (e *Extension) validate(_ *Extension) error {
+	specPath := field.NewPath("spec")
+	var allErrs field.ErrorList
+	// NOTE: ExtensionConfig is behind the RuntimeSDK feature gate flag; the webhook
+	// must prevent creating and updating objects in case the feature flag is disabled.
+	if !feature.Gates.Enabled(feature.RuntimeSDK) {
+		allErrs = append(allErrs, field.Forbidden(
+			specPath,
+			"can be set only if the RuntimeSDK feature flag is enabled",
+		))
+	}
+
+	allErrs = append(allErrs, validateExtensionSpec(e)...)
+
+	if len(allErrs) > 0 {
+		return apierrors.NewInvalid(clusterv1.GroupVersion.WithKind("Extension").GroupKind(), e.Name, allErrs)
+	}
 	return nil
+}
+
+func validateExtensionSpec(e *Extension) field.ErrorList {
+	var allErrs field.ErrorList
+
+	specPath := field.NewPath("spec")
+
+	if e.Spec.ClientConfig.URL == nil && e.Spec.ClientConfig.Service == nil {
+		allErrs = append(allErrs, field.Required(
+			specPath.Child("clientConfig"),
+			"either URL or Service must be defined",
+		))
+	}
+	if e.Spec.ClientConfig.URL != nil && e.Spec.ClientConfig.Service != nil {
+		allErrs = append(allErrs, field.Forbidden(
+			specPath.Child("clientConfig"),
+			"only one of URL or Service can be defined",
+		))
+	}
+
+	// Validate URl
+	if e.Spec.ClientConfig.URL != nil {
+		if _, err := url.ParseRequestURI(*e.Spec.ClientConfig.URL); err != nil {
+			allErrs = append(allErrs, field.Invalid(
+				specPath.Child("clientConfig", "url"),
+				*e.Spec.ClientConfig.URL,
+				"must be a valid URL e.g. https://example.com",
+			))
+		}
+		// TODO: Decide handling of http/https prefixes.
+	}
+
+	// Validate Service if defined
+	if e.Spec.ClientConfig.Service != nil {
+		// Validate that the name is not empty and is a Valid RFC1123 name.
+		if e.Spec.ClientConfig.Service.Name == "" {
+			allErrs = append(allErrs, field.Required(
+				specPath.Child("clientConfig", "service", "name"),
+				"must not be empty",
+			))
+		}
+
+		if errs := validation.IsDNS1123Subdomain(e.Spec.ClientConfig.Service.Name); len(errs) != 0 {
+			allErrs = append(allErrs, field.Invalid(
+				specPath.Child("clientConfig", "service", "name"),
+				e.Spec.ClientConfig.Service.Name,
+				"invalid name",
+			))
+		}
+
+		// TODO: Decide if we need to validate Namespace (should it be equal to some other Namespace?)
+		if e.Spec.ClientConfig.Service.Namespace == "" {
+			allErrs = append(allErrs, field.Required(
+				specPath.Child("clientConfig", "service", "namespace"),
+				"must not be empty",
+			))
+		}
+
+		if e.Spec.ClientConfig.Service.Path != nil {
+			// TODO: Decide if we should be this strict on the path.
+			path := *e.Spec.ClientConfig.Service.Path
+			if _, err := url.ParseRequestURI(path); err != nil {
+				allErrs = append(allErrs, field.Invalid(
+					specPath.Child("clientConfig", "service", "path"),
+					path,
+					"must be a valid URL path e.g. /path/to/hook",
+				))
+			}
+			if path[0:1] != "/" {
+				allErrs = append(allErrs, field.Invalid(
+					specPath.Child("clientConfig", "service", "path"),
+					path,
+					"must be a valid URL path e.g. /path/to/hook",
+				))
+			}
+		}
+		if e.Spec.ClientConfig.Service.Port != nil {
+			if errs := validation.IsValidPortNum(int(*e.Spec.ClientConfig.Service.Port)); len(errs) != 0 {
+				allErrs = append(allErrs, field.Invalid(
+					specPath.Child("clientConfig", "service", "port"),
+					*e.Spec.ClientConfig.Service.Port,
+					"must be in range 1-65535 inclusive",
+				))
+			}
+		}
+	}
+	if e.Spec.NamespaceSelector == nil {
+		allErrs = append(allErrs, field.Required(
+			specPath.Child("NamespaceSelector"),
+			"must be defined",
+		))
+	}
+	return allErrs
 }
 
 // ValidateDelete implements webhook.Validator so a webhook will be registered for the type.
