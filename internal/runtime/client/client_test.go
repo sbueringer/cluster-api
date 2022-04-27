57faffe2e8398ae1e5c721eb5b6e2950540affd7
Christian Schlotter <schlotterc@vmware.com>
2022-04-27 14:25:02 +0200

    more tables

--- internal/runtime/client/client_test.go
+++ internal/runtime/client/client_test.go
@@ -25,6 +25,7 @@ import (
 
 	. "github.com/onsi/gomega"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/utils/pointer"
 	"sigs.k8s.io/cluster-api/internal/runtime/catalog"
 	fakev1alpha1 "sigs.k8s.io/cluster-api/internal/runtime/catalog/test/v1alpha1"
@@ -34,115 +35,131 @@ import (
 func TestClient_httpCall(t *testing.T) {
 	g := NewWithT(t)
 
-	ctx := context.TODO()
-
-	discoveryHookHandler := func(w http.ResponseWriter, r *http.Request) {
-		response := &fakev1alpha1.FakeResponse{
-			TypeMeta: metav1.TypeMeta{
-				Kind:       "DiscoveryHookResponse",
-				APIVersion: fakev1alpha1.GroupVersion.Identifier(),
+	tableTests := []struct {
+		name     string
+		request  runtime.Object
+		response runtime.Object
+		opts     *httpCallOptions
+		wantErr  bool
+	}{
+		{
+			"all nil, err",
+			nil, nil,
+			nil,
+			true,
+		},
+		{
+			"nil catalog, err",
+			&fakev1alpha1.FakeRequest{}, &fakev1alpha1.FakeResponse{},
+			nil,
+			true,
+		},
+		{
+			"empty catalog, err",
+			&fakev1alpha1.FakeRequest{}, &fakev1alpha1.FakeResponse{},
+			&httpCallOptions{
+				catalog: catalog.New(),
 			},
-			Second: "",
-			First:  1,
-		}
-		respBody, err := json.Marshal(response)
-		if err != nil {
-			panic(err)
-		}
-		w.WriteHeader(http.StatusOK)
-		w.Write(respBody)
+			true,
+		},
+		{
+			"ok, no conversion",
+			&fakev1alpha1.FakeRequest{
+				TypeMeta: metav1.TypeMeta{
+					Kind:       "FakeRequest",
+					APIVersion: fakev1alpha1.GroupVersion.Identifier(),
+				},
+			},
+			&fakev1alpha1.FakeResponse{},
+			func() *httpCallOptions {
+				c := catalog.New()
+				hookFunc := func(req *fakev1alpha1.FakeRequest, resp *fakev1alpha1.FakeResponse) {}
+				c.AddHook(
+					fakev1alpha1.GroupVersion,
+					hookFunc,
+					&catalog.HookMeta{},
+				)
+
+				// get same gvh for hook by using the hookfunc and catalog
+				gvh, err := c.GroupVersionHook(hookFunc)
+				g.Expect(err).To(Succeed())
+
+				return &httpCallOptions{
+					catalog: c,
+					gvh:     gvh,
+				}
+			}(),
+			false,
+		},
+		{
+			"ok, conversion",
+			&fakev1alpha2.FakeRequest{
+				TypeMeta: metav1.TypeMeta{
+					Kind:       "FakeRequest",
+					APIVersion: fakev1alpha2.GroupVersion.Identifier(),
+				},
+			},
+			&fakev1alpha2.FakeResponse{},
+			func() *httpCallOptions {
+				c := catalog.New()
+				// register fakev1alpha1 to enable conversion
+				g.Expect(fakev1alpha1.AddToCatalog(c)).To(Succeed())
+				hookFunc := func(req *fakev1alpha1.FakeRequest, resp *fakev1alpha1.FakeResponse) {}
+				c.AddHook(
+					fakev1alpha1.GroupVersion,
+					hookFunc,
+					&catalog.HookMeta{},
+				)
+
+				// get same gvh for hook by using the hookfunc and catalog
+				gvh, err := c.GroupVersionHook(hookFunc)
+				g.Expect(err).To(Succeed())
+
+				return &httpCallOptions{
+					catalog: c,
+					gvh:     gvh,
+				}
+			}(),
+			false,
+		},
 	}
+	for _, tt := range tableTests {
+		t.Run(tt.name, func(t *testing.T) {
+			// a http server is only required if we have a valid catalog, otherwise httpCall will not reach out to the server
+			if tt.opts != nil && tt.opts.catalog != nil {
+				// create http server with fakeHookHandler
+				mux := http.NewServeMux()
+				mux.HandleFunc("/", fakeHookHandler)
+				srv := httptest.NewServer(mux)
+				defer srv.Close()
 
-	t.Run("pass all nil", func(t *testing.T) {
-		g.Expect(httpCall(ctx, nil, nil, nil)).To(HaveOccurred())
-	})
-	t.Run("pass nil catalog", func(t *testing.T) {
-		g.Expect(httpCall(ctx, &fakev1alpha1.FakeRequest{}, &fakev1alpha1.FakeResponse{}, &httpCallOptions{})).To(HaveOccurred())
-	})
-	t.Run("pass empty catalog", func(t *testing.T) {
-		opts := &httpCallOptions{
-			catalog: catalog.New(),
-		}
-		g.Expect(httpCall(ctx, &fakev1alpha1.FakeRequest{}, &fakev1alpha1.FakeResponse{}, opts)).To(HaveOccurred())
-	})
-	t.Run("ok, no conversion, discovery hook", func(t *testing.T) {
-		// create catalog containing a DiscoveryHook
-		c := catalog.New()
-		hookFunc := func(req *fakev1alpha1.FakeRequest, resp *fakev1alpha1.FakeResponse) {}
-		c.AddHook(
-			fakev1alpha1.GroupVersion,
-			hookFunc,
-			&catalog.HookMeta{},
-		)
+				// set url to srv for in tt.opts
+				tt.opts.config.URL = pointer.String(srv.URL)
+			}
 
-		// get same gvh for hook by using the hookfunc
-		gvh, err := c.GroupVersionHook(hookFunc)
-		g.Expect(err).To(Succeed())
+			assert := g.Expect(httpCall(context.TODO(), tt.request, tt.response, tt.opts))
+			if tt.wantErr {
+				assert.To(HaveOccurred())
+			} else {
+				assert.To(Succeed())
+			}
+		})
+	}
+}
 
-		// create opts having gvh same as discovery hook
-		opts := &httpCallOptions{
-			catalog: c,
-			gvh:     gvh,
-		}
-		// use same gvh for req as for hook in catalog
-		req := &fakev1alpha1.FakeRequest{
-			TypeMeta: metav1.TypeMeta{
-				Kind:       "DiscoveryHookRequest",
-				APIVersion: fakev1alpha1.GroupVersion.Identifier(),
-			},
-		}
-		resp := &fakev1alpha1.FakeResponse{}
-
-		// create http server with hook
-		mux := http.NewServeMux()
-		mux.HandleFunc("/", discoveryHookHandler)
-		srv := httptest.NewServer(mux)
-		defer srv.Close()
-
-		// set hook url
-		opts.config.URL = pointer.String(srv.URL)
-
-		g.Expect(httpCall(ctx, req, resp, opts)).To(Succeed())
-	})
-	t.Run("ok, conversion, discovery hook", func(t *testing.T) {
-		// create catalog containing a DiscoveryHook
-		c := catalog.New()
-		// register fakev1alpha2 to enable conversion
-		g.Expect(fakev1alpha2.AddToCatalog(c)).To(Succeed())
-		hookFunc := func(req *fakev1alpha2.FakeRequest, resp *fakev1alpha2.FakeResponse) {}
-		c.AddHook(
-			fakev1alpha2.GroupVersion,
-			hookFunc,
-			&catalog.HookMeta{},
-		)
-
-		// get same gvh for hook by using the hookfunc
-		gvh, err := c.GroupVersionHook(hookFunc)
-		g.Expect(err).To(Succeed())
-
-		// create opts having gvh same as discovery hook
-		opts := &httpCallOptions{
-			catalog: c,
-			gvh:     gvh,
-		}
-		// use same gvh for req as for hook in catalog
-		req := &fakev1alpha1.FakeRequest{
-			TypeMeta: metav1.TypeMeta{
-				Kind:       "FakeRequest",
-				APIVersion: fakev1alpha1.GroupVersion.Identifier(),
-			},
-		}
-		resp := &fakev1alpha1.FakeResponse{}
-
-		// create http server with hook
-		mux := http.NewServeMux()
-		mux.HandleFunc("/", discoveryHookHandler)
-		srv := httptest.NewServer(mux)
-		defer srv.Close()
-
-		// set hook url
-		opts.config.URL = pointer.String(srv.URL)
-
-		g.Expect(httpCall(ctx, req, resp, opts)).To(Succeed())
-	})
+func fakeHookHandler(w http.ResponseWriter, r *http.Request) {
+	response := &fakev1alpha1.FakeResponse{
+		TypeMeta: metav1.TypeMeta{
+			Kind:       "FakeHookResponse",
+			APIVersion: fakev1alpha1.GroupVersion.Identifier(),
+		},
+		Second: "",
+		First:  1,
+	}
+	respBody, err := json.Marshal(response)
+	if err != nil {
+		panic(err)
+	}
+	w.WriteHeader(http.StatusOK)
+	_, _ = w.Write(respBody)
 }
